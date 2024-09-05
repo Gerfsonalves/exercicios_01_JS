e0a1b7d7330289e6aacaacbb40d7ad6eded3a2e8
Gerfson Alves <gerfson17@hotmail.com>
2024-09-05 20:04:57 -0300

    Repetição

---
+++ repeticao.js
@@ -0,0 +1,12 @@
+const paises = ['Alemanha', 'Argentina', 'Brasil', 'México']
+
+// Percorre item por item
+paises.forEach(Element => {
+    console.log('Execução número: ', Element)
+})
+
+// ----------------------------------------------------------
+
+for (let index = 0; index < paises.length; index++) {
+    console.log(paises[index])
+}
