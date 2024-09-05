2533d32576bb15810fa69307571803c763e4054c
Gerfson Alves <gerfson17@hotmail.com>
2024-09-05 17:32:31 -0300

    If Else

---
+++ decisao.js
@@ -0,0 +1,72 @@
+// if - else
+
+const nome = 'Ge'
+
+if (nome == 'ge') { //'nome Ge' 'é igual ==' 'a ge' ?
+    console.log('Condição atendida')
+} else {
+    console.log('Condição não atendida')
+}
+
+// ---------------------------------------------------------------------
+
+const idade = 30
+
+if (idade < 31) { // 'idade 30' 'é menor <' 'que 31' ?
+    console.log('Condição atendida')
+} else {
+    console.log('Condição não atendida')
+}
+
+// ---------------------------------------------------------------------
+
+const saldo = 10
+
+if (saldo > 8) { // 'saldo 10' 'é maior >' 'que 8' ?
+    console.log('Saldo é maior')
+} else {
+    console.log('Saldo é menor')
+}
+
+// ---------------------------------------------------------------------
+
+const preco = 50
+
+if (preco >= 50) { // 'preço 50' 'é maior ou igual >=' 'que 50' ?
+    console.log('Preço é maior ou igual')
+} else {
+    console.log('Preço é menor ou igual')
+}
+
+// ---------------------------------------------------------------------
+
+const dados = 30
+
+if (dados != 50) { // 'dados 30' 'é diferente !=' 'de 50' ?
+    console.log('Sim, é diferente')
+} else {
+    console.log('Não é diferente')
+}
+
+// ---------------------------------------------------------------------
+
+const desconto = 30
+
+if (desconto == 50) { // 
+    console.log('carro 50')
+} else if (desconto == 40) {
+    console.log('carro 40')
+}
+else {
+    console.log('Nenhuma condição')
+}
+
+// ---------------------------------------------------------------------
+
+const e_de_maior = true
+
+if (e_de_maior) {
+    console.log('é maior de idade')
+} else {
+    console.log('não é maior de idade')
+}
