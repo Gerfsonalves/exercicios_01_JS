4b2621a6610acef5a8135950adab08f5aee794c6
Gerfson Alves <gerfson17@hotmail.com>
2024-09-05 20:37:56 -0300

    exercicio 01

---
+++ exe1.js
@@ -0,0 +1,8 @@
+const nomes = ['Eduardo', 'Maria', 'Fernando', 'João', 'Francisco']
+
+nomes.forEach((Element, index) => {
+    console.log('Execução: ', index + 1)
+    console.log('Nome: ',Element)
+    console.log('---------------')
+})
+
