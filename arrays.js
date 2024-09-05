a232b80b279ed5ae4f5bcb2cfc92d11ee1e538cf
Gerfson Alves <gerfson17@hotmail.com>
2024-09-05 16:53:54 -0300

    Arrays

--- arrays.js
+++ arrays.js
@@ -3,3 +3,26 @@ const paises = ['Alemanha', 'Argentina', 'Brasil', 'México']
 console.log(paises[2]) // Selecionar um país específico na lista
 
 console.log(paises.length) // Conta quantos países contém na lista - Tamanho do array
+
+// Adicionar elemento ao final do array
+
+paises.push('Holanda')
+console.log(paises)
+console.log(paises.length)
+
+// Remover último item do array
+
+paises.pop()
+console.log(paises)
+console.log(paises.length)
+
+// Adicionar item no ínicio do array
+
+paises.unshift('França')
+console.log(paises)
+console.log(paises.length)
+
+// Remover item no ínicio do array
+paises.shift()
+console.log(paises)
+console.log(paises.length)
